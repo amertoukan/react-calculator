03a9ee0a6f1f96b4f31ed60f095e328c1a6a1e5d
Amer <toukan.amer@gmail.com>
2019-02-10 16:29:40 -0500

    typo

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require ('express');
 const path = require ('path');
-const PORT = process.env.PORT || 4002; 
+const PORT = process.env.PORT || 4003; 
 
 const app = express(); 
 
-app.use(express.static(__direname))
+app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname,'build')));
 
 app.get('/ping', (req, res) => {
