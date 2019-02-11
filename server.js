ba5de339f290698be6b38830817223f3fb8b2853
Amer <toukan.amer@gmail.com>
2019-02-11 18:48:48 -0500

    heroku build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const path = require ('path');
 const PORT = process.env.PORT || 4003; 
-
+const bodyParser = require ('body-parser')
 const app = express(); 
 
 app.use(express.static(__dirname))
@@ -12,7 +12,7 @@ app.get('/ping', (req, res) => {
 });
 
 
-app.get("/*", (req,res)=>{
+app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname), 'build', index.html)
 });
 
