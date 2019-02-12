73869c6ae77bf407d8a7841b7ed53fbb79696bd4
Amer <toukan.amer@gmail.com>
2019-02-11 19:12:52 -0500

    heroku fix i think

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ping', (req, res) => {
 
 
 app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname), 'build', index.html)
-});
+    res.sendFile(path.join(__dirname, 'build', index.html))}
+    );
 
 app.listen (PORT, ()=>{
     console.log("http://localhost:"+PORT);
