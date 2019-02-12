bf3fb7d62460f307f37f2d85bbe808fcd50be92e
Amer <toukan.amer@gmail.com>
2019-02-11 19:15:17 -0500

    i think this is the one for heroku

---
+++ build/precache-manifest.93146cb5c6c2cb99201a0ddd71a572fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83aad7db13aaa844eb48",
+    "url": "/static/js/main.83aad7db.chunk.js"
+  },
+  {
+    "revision": "b768ae51302df8976cdd",
+    "url": "/static/js/1.b768ae51.chunk.js"
+  },
+  {
+    "revision": "83aad7db13aaa844eb48",
+    "url": "/static/css/main.655e8717.chunk.css"
+  },
+  {
+    "revision": "3635d88fcfe3437b22a8ca4256fe0e4a",
+    "url": "/index.html"
+  }
+];
